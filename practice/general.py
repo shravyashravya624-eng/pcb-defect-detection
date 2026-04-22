34fc1aa8ff363d12e6c7b9943441f0a607a6be62
Shravya <shravyashravya624@gmail.com>
2026-04-22 15:43:28 +0530

    Phase 1 complete: dataset collected and converted to YOLO format (1500 images)

---
+++ practice/general.py
@@ -0,0 +1,31 @@
+print(2+2)
+print(4*3+2-4)
+print('shravya')
+print('shravya'+'jss student')
+print('4'+' '+'toys')
+print('baby'*4)
+print('toes'*4)
+spam=50
+eggs=42
+print(spam+eggs)
+spam=spam+eggs
+print(spam)
+eggs=56
+print(spam+eggs)
+print('hello')
+name=input('what is your name :')
+print('nice to meet you',name)
+print(len(name))
+age=input('whats your age:')
+age=int(age)
+print('my age is',age)
+print('your age after one year is',age+1)
+#type function
+type(42)
+name='shravya'
+type(len(name))
+type(42.999)
+round(333.3399)
+round(333.3399,2)
+
+
